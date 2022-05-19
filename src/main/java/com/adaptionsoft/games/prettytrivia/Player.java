130eda4cbfbd884c6367a08a6800c5dc9c9ce57d
Ariel SAULNIER <arielsaulnierpro@gmail.com>
2022-05-19 12:05:40 +0200

    adding constructor, getters and setters to player class

--- src/main/java/com/adaptionsoft/games/prettytrivia/Player.java
+++ src/main/java/com/adaptionsoft/games/prettytrivia/Player.java
@@ -4,5 +4,21 @@ public class Player {
 
     private String name;
     private Integer prison_count;
-    
+
+    public Player(String name){
+        this.name = name;
+        this.prison_count = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrison_count() {
+        return prison_count;
+    }
+
+    public void setPrison_count(Integer prison_count) {
+        this.prison_count = prison_count;
+    }
 }
--- src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -1,14 +1,17 @@
 package com.adaptionsoft.games.trivia;
 
 
+import com.adaptionsoft.games.prettytrivia.Player;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SomeTest {
 
     @Test
-    public void true_is_true() throws Exception {
-        assertTrue(false);
+    public void prison_count_starts_at_0() throws Exception {
+        Player player = new Player("michel");
+        assertEquals(0,player.getPrisonCount());
     }
 }
